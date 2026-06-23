66bb8b2219d37063ae9c12dd1ae196a5f5292d5f
El Guapo André <andre.nunez@usach.cl>
2026-06-23 18:10:22 -0400

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paquetePip",                                  # Nombre del paquete
+    version="0.1.0",                                    # Versión inicial
+    packages=find_packages(),                           # Paquetes a incluir
+    description="Un paquete pip simple de saludo",      # Breve descripción
+    author="Andre Nuñez",                               # Tu nombre
+    author_email="bushido69nunez@gmail.com",            # Tu correo electrónico
+    url="https://github.com/BushiNunez/paquetePip#",    # URL del proyecto
+)
